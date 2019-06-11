a50ec8fa46c6e94c4e3176d5c801f8b182f6379a
therbtltrmz <48274605+therbtltrmz@users.noreply.github.com>
2019-06-11 22:17:40 +0800

    Update client.java

--- client.java
+++ client.java
@@ -47,7 +47,7 @@ public JList list;
 public JScrollPane scroll;
 private String [] names = {"FREE SERVER", "UNITED STATES","GERMANY","NETHERLANDS","SINGAPORE"};
 private String [] tweaks = {"MTN", "GLO", "ETISALAT", "AIRTEL"};
-public static DefaultListModel dlm = new DefaultListModel();
+public static DefaultListModel dlm = new DefaultListModel();}
 //Main program
 public static void main(String[] args){
     //Draw the main window
@@ -62,7 +62,7 @@ window.setVisible(true);
 window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 window.getContentPane().setBackground(new Color(216,191,216));
 }
- } 
+  
     public VPN(){
     //Specify the values and properties of the various controls and add to JFrame
     panel1 = new JPanel();
